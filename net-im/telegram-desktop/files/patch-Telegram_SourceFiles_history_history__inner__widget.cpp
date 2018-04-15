d5a5c7d0e9852367f121e554836a637faf834885
Henry Hu <henry.hu.sh@gmail.com>
2018-04-15 19:17:41 -0400

    moved file

--- net-im/telegram-desktop/files/patch-Telegram_SourceFiles_history_history__inner__widget.cpp
+++ net-im/telegram-desktop/files/patch-Telegram_SourceFiles_history_history__inner__widget.cpp
@@ -8,11 +8,10 @@
 +#if defined Q_OS_LINUX32 || defined Q_OS_LINUX64 || defined Q_OS_FREEBSD
  	if (!_selected.empty() && _selected.cbegin()->second != FullSelection) {
  		const auto [item, selection] = *_selected.cbegin();
-		if (const auto view = item->mainView()) {
-			SetClipboardWithEntities(
-				view->selectedText(selection),
-				QClipboard::Selection);
-		}
+ 		if (const auto view = item->mainView()) {
+@@ -1345,7 +1345,7 @@ void HistoryInner::mouseActionFinish(
+ 				QClipboard::Selection);
+ 		}
  	}
 -#endif // Q_OS_LINUX32 || Q_OS_LINUX64
 +#endif // Q_OS_LINUX32 || Q_OS_LINUX64 || Q_OS_FREEBSD
