2a62f02b7a8a447c5232e1c228936d5df949e7bb
Henry Hu <henry.hu.sh@gmail.com>
2018-08-01 23:51:32 -0400

    more clever

--- sync.sh
+++ sync.sh
@@ -15,13 +15,22 @@ for makefile in `find . -name Makefile -depth 3`; do
 
     diff_ret=`diff -x work -x '*.orig' /usr/ports/$fullname $portdir`
     if [ $? = 0 ]; then
-        echo "$fullname has no diff"
+        #echo "$fullname has no diff"
         continue
     fi
 
-    echo "$fullname has diff!"
-    diff -ruN -x work -x '*.orig' /usr/ports/$fullname $portdir
+    echo "=== $fullname has diff ==="
+    portver=`cd /usr/ports/$fullname; make -V PORTVERSION`
+    repover=`cd $portdir; make -V PORTVERSION`
+    echo "Port tree version: $portver Repo version: $repover"
+    pkg version -T $portver $repover
+    compare=$?
+    if [ $compare -eq 1 ]; then
+        echo "Repo version is newer. Skip sync."
+        continue
+    fi
 
+    diff -ruN -x work -x '*.orig' /usr/ports/$fullname $portdir
     read -e -p "Sync $fullname with the port version? " answer
-    [ $answer == "y" -o $answer == "Y" ] && cp -r /usr/ports/$fullname/* $portdir/; git add -p $portdir
+    [ $answer == "y" -o $answer == "Y" ] && cp -r /usr/ports/$fullname/* $portdir/ && git add -p $portdir
 done
