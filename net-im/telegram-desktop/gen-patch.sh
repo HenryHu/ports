c20c7081c73e06832feb62470226ed7704391d13
Henry Hu <henry.hu.sh@gmail.com>
2018-08-05 16:30:48 -0400

    add gen patch script

---
+++ net-im/telegram-desktop/gen-patch.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# gen-patch.sh
+# Copyright (C) 2018 henryhu <henryhu@goldpeak>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+diff -ruN -x work -x sync-patch.sh -x gen-patch.sh /usr/ports/net-im/telegram-desktop .
