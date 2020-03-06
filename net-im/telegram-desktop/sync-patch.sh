ace96ef39637f1d6e5e3d1e4d8158c90bd56f907
Henry Hu <henry.hu.sh@gmail.com>
2020-03-05 23:15:20 -0500

    update scripts

--- net-im/telegram-desktop/sync-patch.sh
+++ net-im/telegram-desktop/sync-patch.sh
@@ -1,22 +1,24 @@
 #! /bin/sh
 
 TG_VER=`make -VDISTVERSION`
-LIBTGVOIP_VER=`cat Makefile | grep telegramdesktop:libtgvoip | cut -d : -f 3-3`
+LIBTGVOIP_VER=522550a1
 
 (cd $HOME/proj/tdesktop && git diff v$TG_VER..bsd_$TG_VER > patch)
 (cd $HOME/proj/libtgvoip && git diff $LIBTGVOIP_VER..HEAD > patch)
-rm -f files/patch-* files/gyp-patches
+(cd $HOME/proj/lib_ui && git diff upstream/master master > patch)
+(cd $HOME/proj/lib_base && git diff upstream/master master > patch)
+(cd $HOME/proj/cmake_helpers && git diff upstream/master master > patch)
+rm -f files/patch-*
 make clean
 make extract
 cd work/tdesktop-*
 patch -p1 < $HOME/proj/tdesktop/patch
-cd Telegram/ThirdParty/libtgvoip
-patch -p1 < $HOME/proj/libtgvoip/patch
-cd ../../../../..
+(cd Telegram/ThirdParty/libtgvoip && patch -p1 < $HOME/proj/libtgvoip/patch)
+(cd Telegram/lib_ui && patch -p1 < $HOME/proj/lib_ui/patch)
+(cd Telegram/lib_base && patch -p1 < $HOME/proj/lib_base/patch)
+(cd cmake && patch -p1 < $HOME/proj/cmake_helpers/patch)
+cd ../..
 make makepatch
 cd files
-GYP_FILES=`grep -l %% *`
-cat $GYP_FILES > gyp-patches
-rm $GYP_FILES
 cd ..
 make clean
