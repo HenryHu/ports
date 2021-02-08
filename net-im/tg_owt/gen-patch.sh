c535adaa062aef6c121353b867dbeecf8e9dd5e3
Henry Hu <henry.hu.sh@gmail.com>
2021-02-07 22:33:10 -0500

    script to gen patch

---
+++ net-im/tg_owt/gen-patch.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# gen-patch.sh
+# Copyright (C) 2018 henryhu <henryhu@goldpeak>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+diff -ruN -x work -x sync-patch.sh -x gen-patch.sh -x update.md -x check-diff.py -x files.bak /usr/ports/net-im/tg_owt .
