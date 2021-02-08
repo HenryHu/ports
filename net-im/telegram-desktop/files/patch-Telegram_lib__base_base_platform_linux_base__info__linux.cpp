f6f54d2134bd8c91597346bd74488dcf87707bd9
Henry Hu <henry.hu.sh@gmail.com>
2021-02-07 22:33:00 -0500

    update patch

--- net-im/telegram-desktop/files/patch-Telegram_lib__base_base_platform_linux_base__info__linux.cpp
+++ net-im/telegram-desktop/files/patch-Telegram_lib__base_base_platform_linux_base__info__linux.cpp
@@ -12,58 +12,30 @@
  namespace Platform {
  namespace {
  
-@@ -100,6 +95,15 @@ QString DeviceModelPretty() {
- }
- 
- QString SystemVersionPretty() {
-+<<<<<<< HEAD
-+	const auto result = getenv("XDG_CURRENT_DESKTOP");
-+	const auto value = result ? QString::fromLatin1(result) : QString();
-+	const auto list = value.split(':', QString::SkipEmptyParts);
-+
-+	return "FreeBSD "
-+		+ (list.isEmpty() ? QString() : list[0] + ' ')
-+		+ (IsWayland() ? "Wayland " : "X11 ");
-+=======
+@@ -103,7 +98,7 @@ QString SystemVersionPretty() {
  	static const auto result = [&] {
  		QStringList resultList{};
  
-@@ -138,6 +142,7 @@ QString SystemVersionPretty() {
- 	}();
- 
- 	return result;
-+>>>>>>> 2bf29ab1a5458003c8ed250886e08c61cce5ff72
+-#ifdef Q_OS_LINUX
++#if 0
+ 		resultList << "Linux";
+ #else // Q_OS_LINUX
+ 		resultList << QSysInfo::kernelType();
+@@ -186,7 +181,7 @@ QString AutoUpdateKey() {
  }
  
- QString SystemCountry() {
-@@ -158,10 +163,13 @@ QDate WhenSystemBecomesOutdated() {
- 
- 	if (IsLinux32Bit()) {
- 		return QDate(2020, 9, 1);
-+<<<<<<< HEAD
-+=======
- 	} else if (libcName == qstr("glibc") && !libcVersion.isEmpty()) {
- 		if (QVersionNumber::fromString(libcVersion) < QVersionNumber(2, 23)) {
- 			return QDate(2020, 9, 1); // Older than Ubuntu 16.04.
- 		}
-+>>>>>>> 2bf29ab1a5458003c8ed250886e08c61cce5ff72
- 	}
- 
- 	return QDate();
-@@ -185,6 +193,8 @@ QString AutoUpdateKey() {
- 	}
- }
- 
-+<<<<<<< HEAD
-+=======
  QString GetLibcName() {
- #ifdef Q_OS_LINUX
+-#ifdef Q_OS_LINUX
++#if 0
  	return "glibc";
-@@ -205,6 +215,7 @@ QString GetLibcVersion() {
- 	return QString();
+ #endif // Q_OS_LINUX
+ 
+@@ -194,7 +189,7 @@ QString GetLibcName() {
  }
  
-+>>>>>>> 2bf29ab1a5458003c8ed250886e08c61cce5ff72
- bool IsWayland() {
- 	return QGuiApplication::instance()
- 		? QGuiApplication::platformName().startsWith(
+ QString GetLibcVersion() {
+-#ifdef Q_OS_LINUX
++#if 0
+ 	static const auto result = [&] {
+ 		const auto version = QString::fromLatin1(gnu_get_libc_version());
+ 		return QVersionNumber::fromString(version).isNull() ? QString() : version;
