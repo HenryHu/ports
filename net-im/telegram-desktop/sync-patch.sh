bf8291b7d2c0443a8a24c7b4e0ef6d55a6b771e1
Henry Hu <henry.hu.sh@gmail.com>
2021-10-08 14:59:19 -0400

    update sync patch

--- net-im/telegram-desktop/sync-patch.sh
+++ net-im/telegram-desktop/sync-patch.sh
@@ -4,19 +4,24 @@ TG_VER=`make -VDISTVERSION`
 # version of Telegram/ThirdParty/libtgvoip submodule.
 LIBTGVOIP_VER=373e416
 # version of cmake submodule.
-CMAKE_VER=dbc1e4f
+CMAKE_VER=13575af
 # latest version of tg_owt
 # OWT_VER=`cat Makefile | grep desktop-app:tg_owt | cut -d : -f 3-3 `
-LIBBASE_VER=1db6935
+LIBBASE_VER=e4b41e9
 
 MY_VER=bsd_$TG_VER
 
 (cd $HOME/proj/tdesktop && git diff v$TG_VER..$MY_VER > patch)
 (cd $HOME/proj/libtgvoip && git diff $LIBTGVOIP_VER..HEAD > patch)
 (cd $HOME/proj/lib_ui && git diff upstream/master master > patch)
-(cd $HOME/proj/lib_base && git diff $LIBBASE_VER $TG_VER > patch)
+(cd $HOME/proj/lib_base && git diff $LIBBASE_VER $MY_VER > patch)
 (cd $HOME/proj/cmake_helpers && git diff $CMAKE_VER $MY_VER > patch)
 #(cd $HOME/proj/tg_owt && git diff $OWT_VER $MY_VER > patch)
+
+mkdir /tmp/patch_backup
+mv files/patch-*tgcalls* /tmp/patch_backup
+mv files/patch-*lib__webview* /tmp/patch_backup
+
 rm -f files/patch-*
 make clean
 make extract
@@ -36,3 +41,5 @@ rm Telegram/ThirdParty/hunspell/tests/suggestiontest/Makefile.orig
 cd ../..
 make makepatch
 make clean
+
+mv /tmp/patch_backup/* files/
